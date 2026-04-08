0d410ce605d08342b5f6225190b2c68f0c699f1b
Weebo <101129577+Weebo64@users.noreply.github.com>
2026-04-08 22:38:39 +0200

    uwu
    new pause menu by Ro and THF/IKW-Team

--- PulsarEngine/Race/MiscRace.cpp
+++ PulsarEngine/Race/MiscRace.cpp
@@ -61,11 +61,9 @@ kmCall(0x807eb154, MiiHeads);
 kmWrite32(0x807eb15c, 0x60000000);
 kmWrite32(0x807eb160, 0x88de01b4);
 
-//credit to XeR for finding the float address
-static void BattleGlitchEnable() {
-    const u8 val = Settings::Mgr::Get().GetSettingValue(Settings::SETTINGSTYPE_RACE, SETTINGRACE_RADIO_BATTLE);
+//Battle Glitch removed - names always visible at normal distance
+static void SetNameDistance() {
     float maxDistance = 7500.0f;
-    if (val == RACESETTING_BATTLE_GLITCH_ENABLED) maxDistance = 75000.0f;
     System* system = System::sInstance;
     if (system->IsContext(PULSAR_MODE_OTT)) {
         const Input::RealControllerHolder* controllerHolder = SectionMgr::sInstance->pad.padInfos[0].controllerHolder;
@@ -89,7 +87,7 @@ static void BattleGlitchEnable() {
     }
     RaceBalloons::maxDistanceNames = maxDistance;
 }
-RaceFrameHook BattleGlitch(BattleGlitchEnable);
+RaceFrameHook NameDistance(SetNameDistance);
 
 kmWrite32(0x8085C914, 0x38000000); //times at the end of races in VS
 static void DisplayTimesInsteadOfNames(CtrlRaceResult& result, u8 id) {
@@ -149,5 +147,76 @@ kmCall(0x807ef3e0, ChangeItemWindowPane);
 kmCall(0x807ef444, ChangeItemWindowPane);
 
 kmWrite24(0x808A9FF3, 'PUL');
+
+//Enhanced Pause Menu (Ro/Insane Kart Wii)
+//replaces all pause menus with TT pause so you can restart in GP/VS/Battle
+kmWrite32(0x8062c658, 0x38800019);
+kmWrite32(0x8062c79c, 0x38800019);
+kmWrite32(0x80633a98, 0x38600019);
+kmWrite32(0x8062c8e0, 0x38800019);
+kmWrite32(0x80633970, 0x38600019);
+kmWrite32(0x8083d618, 0x60000000);
+
+extern "C" void sInstance__8Racedata(void*);
+extern "C" void sInstance__10SectionMgr(void*);
+asmFunc EnhancedPauseMenu1() {
+    ASM(
+    lwz r3, sInstance__10SectionMgr@l (r6);
+    lwz r12, sInstance__8Racedata@l (r6);
+    lwz r0, 0x1760 (r12);
+    cmpwi r0, 2;
+    beq end;
+
+    cmpwi r4, 0x49;
+    beq decreaseRaceNum;
+
+    cmpwi r4, 0x4A;
+    bne end;
+
+    li r4, 0x4B;
+
+    cmpwi r0, 1;
+    beq decreaseRaceNum;
+
+    li r4, 0x4C;
+
+    decreaseRaceNum:
+    lwz r6, 0x98 (r3);
+    lwz r31, 0x60 (r6);
+    subi r31, r31, 1;
+    stw r31, 0x60 (r6);
+
+    li r31, 5;
+    stw r31, 0x1764 (r12);
+
+    end:
+    mr r31, r5;
+    blr;
+    )
+}
+kmCall(0x806024d8, EnhancedPauseMenu1);
+
+asmFunc EnhancedPauseMenu2() {
+    ASM(
+    lis r3, sInstance__8Racedata@ha;
+    lwz r3, sInstance__8Racedata@l (r3);
+    lwz r4, 0x1760 (r3);
+    cmpwi r4, 2;
+    beq end;
+    li r4, 0x1;
+    stw r4, 0xD18 (r3);
+    stw r4, 0xE08 (r3);
+    stw r4, 0xEF8 (r3);
+
+    end:
+    li        r3, 0x6C4;
+    blr;
+    )
+}
+kmCall(0x80623df4, EnhancedPauseMenu2);
+
+kmWrite32(0x80859068, 0x48000808);
+kmWrite32(0x80858e38, 0x48000040);
+
 }//namespace Race
 }//namespace Pulsar
