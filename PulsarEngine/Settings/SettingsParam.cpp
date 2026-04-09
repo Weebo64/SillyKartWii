5336ef7fbf870eb6c6376fbb446e0428490269c1
Weebo <101129577+Weebo64@users.noreply.github.com>
2026-04-09 21:56:04 +0200

    Add Input Viewer to Menu Settings

--- PulsarEngine/Settings/SettingsParam.cpp
+++ PulsarEngine/Settings/SettingsParam.cpp
@@ -8,29 +8,27 @@ namespace Pulsar {
 namespace Settings {
 
 u8 Params::radioCount[Params::pageCount] ={
-    4, 6, 3, 5, 2 //menu, race, host, OTT, KO
-    //Add user radio count here
+    5, 5, 3, 5, 2
 
 };
-u8 Params::scrollerCount[Params::pageCount] ={ 1, 1, 1, 0, 2 }; //menu, race, host, OTT, KO
+u8 Params::scrollerCount[Params::pageCount] ={ 1, 1, 1, 0, 2 };
 
-u8 Params::buttonsPerPagePerRow[Params::pageCount][Params::maxRadioCount] = //first row is PulsarSettingsType, 2nd is rowIdx of radio
+u8 Params::buttonsPerPagePerRow[Params::pageCount][Params::maxRadioCount] =
 {
-    { 2, 2, 3, 2, 0, 0, 0, 0 }, //Menu 
-    { 2, 2, 2, 2, 2, 3, 0, 0 }, //Race
-    { 3, 3, 2, 2, 2, 0, 0, 0 }, //OTT
-    { 2, 2, 0, 0, 0, 0, 0, 0 }, //KO
-    //{}, //User
+    { 2, 2, 3, 2, 2, 0, 0, 0 },
+    { 2, 2, 2, 2, 3, 0, 0, 0 },
+    { 2, 4, 2, 0, 0, 0, 0, 0 },
+    { 3, 3, 2, 2, 2, 0, 0, 0 },
+    { 2, 2, 0, 0, 0, 0, 0, 0 },
 };
 
 u8 Params::optionsPerPagePerScroller[Params::pageCount][Params::maxScrollerCount] =
 {
-    { 5, 0, 0, 0, 0, 0, 0, 0}, //Menu
-    { 4, 0, 0, 0, 0, 0, 0, 0}, //Race
-    { 7, 0, 0, 0, 0, 0, 0, 0}, //Host
-    { 0, 0, 0, 0, 0, 0, 0, 0}, //OTT
-    { 4, 4, 0, 0, 0, 0, 0, 0}, //KO
-    //{}, //User
+    { 5, 0, 0, 0, 0, 0, 0, 0},
+    { 4, 0, 0, 0, 0, 0, 0, 0},
+    { 7, 0, 0, 0, 0, 0, 0, 0},
+    { 0, 0, 0, 0, 0, 0, 0, 0},
+    { 4, 4, 0, 0, 0, 0, 0, 0},
 };
 
 }//namespace Settings
