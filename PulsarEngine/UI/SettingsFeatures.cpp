3653487fa3d0d778226a47fe0d0560d1d283c7af
Weebo <101129577+Weebo64@users.noreply.github.com>
2026-05-30 21:33:13 +0100

    Add files via upload

---
+++ PulsarEngine/UI/SettingsFeatures.cpp
@@ -0,0 +1,39 @@
+#include <kamek.hpp>
+#include <Settings/SettingsParam.hpp>
+#include <PulsarSystem.hpp>
+#include <MarioKartWii/UI/Section/SectionMgr.hpp>
+#include <MarioKartWii/RKNet/RKNetController.hpp>
+#include <Gamemodes/FreeRoam/FRMgr.hpp>
+
+namespace Pulsar {
+namespace UI {
+
+void GameModeToggle() {
+    if(Racedata::sInstance->menusScenario.settings.gamemode != MODE_TIME_TRIAL) FreeRoam::U16_FREE_ROAM = 0x0000;
+
+    SectionId id = SectionMgr::sInstance->nextSectionId;
+    if(id == SECTION_SINGLE_P_BT_NEXT_BATTLE) {
+        id = SECTION_SINGLE_P_VS_NEXT_RACE;
+        SectionMgr::sInstance->nextSectionId = id;
+    }
+    if(id == SECTION_AWARD_38) {
+        id = SECTION_MAIN_MENU_FROM_MENU;
+        SectionMgr::sInstance->nextSectionId = id;
+    }
+    if(Racedata::sInstance->menusScenario.settings.gamemode != MODE_GRAND_PRIX && id == SECTION_GP_INTRO) {
+        id = SECTION_VS_RACE_INTRO;
+        SectionMgr::sInstance->nextSectionId = id;
+    }
+
+    if(Racedata::sInstance->menusScenario.settings.gamemode == MODE_TIME_TRIAL) return;
+    if(Racedata::sInstance->menusScenario.settings.gamemode == MODE_MISSION_TOURNAMENT) return;
+    
+    bool isWWVS = Racedata::sInstance->menusScenario.settings.gamemode == MODE_PUBLIC_VS || Racedata::sInstance->menusScenario.settings.gamemode == MODE_PRIVATE_VS;
+    bool isWWBT = Racedata::sInstance->menusScenario.settings.gamemode == MODE_PUBLIC_BATTLE || Racedata::sInstance->menusScenario.settings.gamemode == MODE_PRIVATE_BATTLE;
+    if(isWWVS || isWWBT) return;
+    System::sInstance->UpdateContext();
+}
+static PageLoadHook2 GAMEMODE(GameModeToggle);
+
+}
+}
