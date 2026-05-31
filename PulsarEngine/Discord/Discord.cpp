bf5d985f9348e37141ddd2b7b48a269b267ea757
Weebo <101129577+Weebo64@users.noreply.github.com>
2026-05-31 19:04:12 +0100

    Enhance Discord Rich Presence functionality
    Updated Discord Rich Presence integration for Silly Kart Wii, including improved handling of game states and player counts.

--- PulsarEngine/Discord/Discord.cpp
+++ PulsarEngine/Discord/Discord.cpp
@@ -1,3 +1,29 @@
+/*
+ * Discord Rich Presence Integration for Silly Kart Wii
+ * 
+ * This file is released as open source under the MIT License.
+ * 
+ * Copyright (c) 2024 Silly Kart Wii Team
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <SillyKartWii.hpp>
 #include <Discord/Discord.hpp>
 #include <Dolphin/DolphinIOS.hpp>
@@ -42,13 +68,13 @@ void DiscordRichPresence(Section* _this) {
     }
 
     if (!hasWrittenClientID) {
-        Dolphin::SetDiscordClient("1493251523579347076");
+        Dolphin::SetDiscordClient("YOUR CLIENT ID HERE");
         hasWrittenClientID = true;
     }
 
     char* state = "";
     char* details = "In a Menu";
-    char* largeImageText = "Silly Kart Wii";
+    char* largeImageText = "YOUR MOD PACK";
     int minPlayers = 0;
     int maxPlayers = 0;
 
@@ -192,7 +218,7 @@ void DiscordRichPresence(Section* _this) {
     Dolphin::SetDiscordPresence(
         details,
         state,
-        "skw_icon",
+        "",
         largeImageText,
         "",
         "",
