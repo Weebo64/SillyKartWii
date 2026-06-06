d2a992b6400ebfd4d6c40b482b04d1339dff884f
Weebo <101129577+Weebo64@users.noreply.github.com>
2026-06-06 16:23:43 +0100

    Uncomment instant POW block write operation

--- PulsarEngine/Race/MiscRace.cpp
+++ PulsarEngine/Race/MiscRace.cpp
@@ -164,7 +164,7 @@ kmWrite32(0x80633970, 0x38600019);
 kmWrite32(0x8083d618, 0x60000000);
 
 //Instant POW Block (IKW)
-//kmWrite16(0x807B1DD2, 0x00000080);
+kmWrite16(0x807B1DD2, 0x00000080);
 
 extern "C" void sInstance__8Racedata(void*);
 extern "C" void sInstance__10SectionMgr(void*);
