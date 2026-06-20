91bcde95a4ad2cdef17b1eccf09ff2fcbf387018
Weebo <101129577+Weebo64@users.noreply.github.com>
2026-06-20 16:46:56 +0100

    Add files via upload

--- PulsarEngine/Settings/SettingsParam.cpp
+++ PulsarEngine/Settings/SettingsParam.cpp
@@ -8,19 +8,19 @@ namespace Pulsar {
 namespace Settings {
 
 u8 Params::radioCount[Params::pageCount] ={
-    5, 5, 4, 5, 2, 4, 2, 4
+    5, 5, 3, 5, 2, 4, 4, 4
 };
 u8 Params::scrollerCount[Params::pageCount] ={ 1, 1, 1, 0, 2, 0, 1, 0 };
 
 u8 Params::buttonsPerPagePerRow[Params::pageCount][Params::maxRadioCount] =
 {
-    { 2, 2, 3, 2, 2, 0, 0, 0 },
+    { 3, 2, 3, 2, 2, 0, 0, 0 },
     { 2, 2, 2, 2, 3, 0, 0, 0 },
-    { 2, 4, 2, 2, 0, 0, 0, 0 },
+    { 2, 4, 2, 0, 0, 0, 0, 0 },
     { 3, 3, 2, 2, 2, 0, 0, 0 },
     { 2, 2, 0, 0, 0, 0, 0, 0 },
     { 2, 2, 2, 2, 0, 0, 0, 0 },
-    { 2, 2, 0, 0, 0, 0, 0, 0 },
+    { 2, 2, 2, 2, 0, 0, 0, 0 },
     { 2, 2, 2, 2, 0, 0, 0, 0 },
 };
 
