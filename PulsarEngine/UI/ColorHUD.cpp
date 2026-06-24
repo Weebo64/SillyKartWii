c35412fc7382720156d592e065801e816f37e390
Weebo <101129577+Weebo64@users.noreply.github.com>
2026-06-24 18:39:48 +0100

    Add files via upload

--- PulsarEngine/UI/ColorHUD.cpp
+++ PulsarEngine/UI/ColorHUD.cpp
@@ -7,43 +7,31 @@ namespace UI {
 static u8 hudR = 255;
 static u8 hudG = 255;
 static u8 hudB = 255;
-static bool colorInitialized = false;
 
-static const u8 hudColors[13][3] = {
+static const u8 hudColors[15][3] = {
     {255, 255, 255}, // White
     {60, 60, 60},    // Dark Gray/Black
     {198, 0, 0},     // Red
     {240, 136, 10},   // Orange
     {245, 200, 20},   // Yellow
     {2, 95, 2},     // Green
+    {76, 255, 0},   // Lime
     {8, 39, 205},    // Blue
     {70, 15, 150},   // Purple
     {235, 105, 210}, // Pink
-    {230, 0, 230},   // Magenta
+    {127, 51, 0},   // Brown
     {36, 167, 240},  // Cyan
     {0, 160, 145},   // Teal
-    {207, 160, 45}   // Gold
+    {207, 160, 45},   // Gold
+    {176, 11, 105} // funny boobies 69 color XDDDDDDDDDDDDDD (#b00b69)
 };
 
 void UpdateHUDColor() {
-    static u8 lastSetting = 255;
     u8 setting = Settings::Mgr::Get().GetSettingValue(Settings::SETTINGSTYPE_MISC, SETTINGMISC_SCROLL_HUDCOLOR);
-    
-    if (!colorInitialized || setting != lastSetting) {
-        // #b00b69 hehe xDDDDDDDDDDDDDDDDDD
-        if (setting == 13) {
-            hudR = 176;
-            hudG = 11;
-            hudB = 105;
-        } else {
-            if (setting > 13) setting = 0;
-            hudR = hudColors[setting][0];
-            hudG = hudColors[setting][1];
-            hudB = hudColors[setting][2];
-        }
-        lastSetting = setting;
-        colorInitialized = true;
-    }
+    if (setting >= 15   ) setting = 0;
+    hudR = hudColors[setting][0];
+    hudG = hudColors[setting][1];
+    hudB = hudColors[setting][2];
 }
 
 void GetHUDColor(void* self, RGBA16* c0, RGBA16* c1) {
@@ -69,5 +57,22 @@ void GetHUDBaseColor(void* self, RGBA16* c) {
 }
 kmBranch(0x805f04d8, GetHUDBaseColor);
 
+void GetHUDRaceColor(nw4r::lyt::Pane* _this, u32 idx, nw4r::ut::Color color) {
+    UpdateHUDColor();
+    if (idx < 2) {
+        color.r = hudR;
+        color.g = hudG;
+        color.b = hudB;
+        color.a = 0xFD;
+    } else {
+        color.r = hudR > 20 ? hudR - 20 : 0;
+        color.g = hudG > 20 ? hudG - 20 : 0;
+        color.b = hudB > 20 ? hudB - 20 : 0;
+        color.a = 0xFD;
+    }
+    _this->SetVtxColor(idx, color);
 }
-}
+kmCall(0x807ec1dc, GetHUDRaceColor);
+
+}  // namespace UI
+}  // namespace Pulsar
